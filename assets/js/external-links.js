5fd8d8200e90a8e0a6f31cccbd6c8cef048df157
Ruthvik Bommena <110110459+ruthvik-bommena@users.noreply.github.com>
2025-04-05 15:23:11 +0530

    Update external-links.js

--- assets/js/external-links.js
+++ assets/js/external-links.js
@@ -2,10 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
   var anchors = document.getElementsByTagName("a");
   for (var i = 0; i < anchors.length; i++) {
     var a = anchors[i];
-    // Check if the link is external by comparing hostnames
+    // If the link is external, add target="_blank" and rel attributes
     if (a.href && a.hostname !== window.location.hostname && a.href.indexOf("http") === 0) {
       a.target = "_blank";
       a.rel = "noopener noreferrer";
     }
   }
 });
+
